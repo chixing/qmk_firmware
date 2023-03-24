2883abf1f380767ae54195deb8e6c8113ec740f9
chixing <xingchi90@gmail.com>
2023-03-24 12:19:27 -0700

    caps layer and volume buttons

--- keyboards/kbdfans/kbd67/mkiirgb/keymaps/chixing/keymap.c
+++ keyboards/kbdfans/kbd67/mkiirgb/keymaps/chixing/keymap.c
@@ -3,16 +3,17 @@
 #define _LAYER1 1
 #define _EMPTY 2
 #define _NUMPAD 3
+#define _CAPSLOCK 4
 
 // layering on mac is uses karabiner to accomadate defualt keyboard
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		[_LAYER0] = LAYOUT_65_ansi_blocker( /* Base */
-			KC_GRV,        KC_1,          KC_2,          KC_3,         KC_4,          KC_5,  KC_6,    KC_7,         KC_8,            KC_9,            KC_0,            KC_MINS,  KC_EQL,  KC_BSPC,   MO(1),
-		    KC_TAB,        KC_Q,          KC_W,          KC_E,         KC_R,          KC_T,  KC_Y,    KC_U,         KC_I,            KC_O,            KC_P,            KC_LBRC,  KC_RBRC, KC_BSLS,   MO(2),
-			KC_CAPS,       KC_A,          KC_S,          LT(3, KC_D),  KC_F,          KC_G,  KC_H,    KC_J,         KC_K,            KC_L,            KC_SCLN,         KC_QUOT,           KC_ENT,    KC_PGUP,
-		    KC_LSFT,       LCTL_T(KC_Z),  LSFT_T(KC_X),  LALT_T(KC_C), LGUI_T(KC_V),  KC_B,  KC_N,    LGUI_T(KC_M), LALT_T(KC_COMM), LSFT_T(KC_DOT),  LCTL_T(KC_SLSH), KC_RSFT,           KC_UP,     KC_PGDN,
-		    KC_LCTL,       KC_LALT,       KC_LGUI,                     KC_SPC,                        KC_RGUI,      KC_RALT,                                           KC_LEFT,           KC_DOWN,   KC_RIGHT),
+			KC_GRV,         KC_1,          KC_2,          KC_3,         KC_4,          KC_5,  KC_6,    KC_7,         KC_8,            KC_9,            KC_0,            KC_MINS,  KC_EQL,  KC_BSPC,   MO(1),
+		    KC_TAB,         KC_Q,          KC_W,          KC_E,         KC_R,          KC_T,  KC_Y,    KC_U,         KC_I,            KC_O,            KC_P,            KC_LBRC,  KC_RBRC, KC_BSLS,   MO(2),
+			LT(4, KC_CAPS), KC_A,          KC_S,          LT(3, KC_D),  KC_F,          KC_G,  KC_H,    KC_J,         KC_K,            KC_L,            KC_SCLN,         KC_QUOT,           KC_ENT,    KC_PGUP,
+		    KC_LSFT,        LCTL_T(KC_Z),  LSFT_T(KC_X),  LALT_T(KC_C), LGUI_T(KC_V),  KC_B,  KC_N,    LGUI_T(KC_M), LALT_T(KC_COMM), LSFT_T(KC_DOT),  LCTL_T(KC_SLSH), KC_RSFT,           KC_UP,     KC_PGDN,
+		    KC_LCTL,        KC_LALT,       KC_LGUI,                     KC_SPC,                        KC_RGUI,      KC_RALT,                                           KC_LEFT,           KC_DOWN,   KC_RIGHT),
 		[_LAYER1] = LAYOUT_65_ansi_blocker( /* FN */
 			KC_TRNS,  KC_F1,    KC_F2,   KC_F3,  KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  KC_DEL,   KC_TRNS,
 		    KC_TRNS,  RGB_TOG,  RGB_MOD, RGB_HUI,RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS, KC_PSCR, KC_SCRL,  KC_PAUS, QK_BOOT,  KC_TRNS,
@@ -27,8 +28,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TRNS,  KC_TRNS,  KC_TRNS,                     KC_TRNS,                   KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS,  KC_TRNS),
         [_NUMPAD] = LAYOUT_65_ansi_blocker(
             KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
-            KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_7,    KC_8,    KC_9,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
-            KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_4,    KC_5,    KC_6,    KC_TRNS, KC_TRNS,          KC_TRNS,  KC_TRNS,
+            KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,  KC_VOLU,  KC_BRIU, KC_PSLS, KC_7,    KC_8,    KC_9,    KC_PMNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
+            KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,  KC_VOLD,  KC_BRID, KC_PAST, KC_4,    KC_5,    KC_6,    KC_PPLS, KC_TRNS,          KC_TRNS,  KC_TRNS,
             KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_0,    KC_1,    KC_2,    KC_3,    KC_TRNS, KC_TRNS,          KC_TRNS,  KC_TRNS,
             KC_TRNS,  KC_TRNS,  KC_TRNS,                     KC_TRNS,                   KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS,  KC_TRNS),
+        [_CAPSLOCK] = LAYOUT_65_ansi_blocker(
+            KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_TRNS,       KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
+            KC_TRNS,  KC_TRNS,  KC_TRNS,     C(S(KC_TAB)),  C(KC_TAB), KC_TRNS,   KC_TRNS, KC_BSPC, KC_DEL,  KC_TRNS, KC_PGUP, KC_HOME, KC_TRNS, KC_TRNS,  KC_TRNS,
+            KC_TRNS,  KC_TRNS,  A(KC_LEFT),  A(KC_RIGHT),   KC_TRNS,   KC_TRNS,   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_PGDN, KC_END,           KC_TRNS,  KC_TRNS,
+            KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_TRNS,       KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,  KC_TRNS,
+            KC_TRNS,  KC_TRNS,  KC_TRNS,                               KC_TRNS,                     KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS,  KC_TRNS),
 };
