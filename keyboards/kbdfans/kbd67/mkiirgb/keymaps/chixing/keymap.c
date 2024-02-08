b758b5cd2cff443f9accb256241c0c45ac8e09ef
Chi Xing <xingchi90@gmail.com>
2024-02-08 02:38:57 -0800

    cannot switch from layer 8 to 2

--- keyboards/kbdfans/kbd67/mkiirgb/keymaps/chixing/keymap.c
+++ keyboards/kbdfans/kbd67/mkiirgb/keymaps/chixing/keymap.c
@@ -9,7 +9,13 @@
 #define _SYMBOLS_LEFT 7
 #define _GAMING 8
 
+// this is the kbd keyboard (black clear bottom)
+
 // layering on mac is uses karabiner to accomadate defualt keyboard
+
+// base layer is mac with layer switching
+// windows mode just switches alt and win
+// game mode disables layers
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		[_MAC] = LAYOUT_65_ansi_blocker( /* Base */
@@ -61,9 +67,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TRNS,  KC_PERC,  KC_CIRC, KC_AMPR,  KC_ASTR,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,  KC_TRNS,
             KC_TRNS,  KC_TRNS,  KC_TRNS,                     KC_TRNS,                   KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS,  KC_TRNS),
 		[_GAMING] = LAYOUT_65_ansi_blocker( /* Base */
-			KC_GRV,         KC_1,          KC_2,          KC_3,         KC_4,          KC_5,  KC_6,    KC_7,         KC_8,            KC_9,            KC_0,            KC_MINS,  KC_EQL,  KC_BSPC,   TG(2),
+			KC_GRV,         KC_1,          KC_2,          KC_3,         KC_4,          KC_5,  KC_6,    KC_7,         KC_8,            KC_9,            KC_0,            KC_MINS,  KC_EQL,  KC_BSPC,   KC_ESC,
 		    KC_TAB,         KC_Q,          KC_W,          KC_E,         KC_R,          KC_T,  KC_Y,    KC_U,         KC_I,            KC_O,            KC_P,            KC_LBRC,  KC_RBRC, KC_BSLS,   TG(8),
-			LT(4, KC_CAPS), KC_A,          KC_S,          KC_D,         KC_F,          KC_G,  KC_H,    KC_J,         KC_K,            KC_L,            KC_SCLN,         KC_QUOT,           KC_ENT,    KC_PGUP,
+			KC_CAPS,        KC_A,          KC_S,          KC_D,         KC_F,          KC_G,  KC_H,    KC_J,         KC_K,            KC_L,            KC_SCLN,         KC_QUOT,           KC_ENT,    KC_PGUP,
 		    KC_LSFT,        KC_Z,          KC_X,          KC_C,         KC_V,          KC_B,  KC_N,    KC_M,         KC_COMM,         KC_DOT,          KC_SLSH,         KC_RSFT,           KC_UP,     KC_PGDN,
-		    KC_LCTL,        KC_LGUI,       KC_LALT,                     KC_SPC,                        KC_RGUI,      MO(1),                                             KC_LEFT,           KC_DOWN,   KC_RIGHT),
+		    KC_LCTL,        KC_LGUI,       KC_LALT,                     KC_SPC,                        KC_RGUI,      KC_RCTL,                                           KC_LEFT,           KC_DOWN,   KC_RIGHT),
 };
+
