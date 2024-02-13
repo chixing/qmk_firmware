8238b1a6d7296ce46987ae1fa11eed24e0f30aa5
Chi Xing <xingchi90@gmail.com>
2024-02-13 09:04:05 -0800

    repalce pg up/dn with volume

--- keyboards/kbdfans/kbd67/mkiirgb/keymaps/chixing/keymap.c
+++ keyboards/kbdfans/kbd67/mkiirgb/keymaps/chixing/keymap.c
@@ -21,8 +21,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		[_MAC] = LAYOUT_65_ansi_blocker( /* Base */
 			KC_GRV,         KC_1,          KC_2,          KC_3,         KC_4,          KC_5,  KC_6,    KC_7,         KC_8,            KC_9,            KC_0,            KC_MINS,  KC_EQL,  KC_BSPC,   TG(2),
 		    KC_TAB,         KC_Q,          KC_W,          KC_E,         KC_R,          KC_T,  KC_Y,    KC_U,         KC_I,            KC_O,            KC_P,            KC_LBRC,  KC_RBRC, KC_BSLS,   TG(8),
-			LT(4, KC_CAPS), KC_A,          LT(5, KC_S),   LT(3, KC_D),  LT(6, KC_F),   KC_G,  KC_H,    KC_J,         KC_K,            KC_L,            LT(7, KC_SCLN),  KC_QUOT,           KC_ENT,    KC_PGUP,
-		    KC_LSFT,        LCTL_T(KC_Z),  LSFT_T(KC_X),  LALT_T(KC_C), LGUI_T(KC_V),  KC_B,  KC_N,    LGUI_T(KC_M), LALT_T(KC_COMM), LSFT_T(KC_DOT),  LCTL_T(KC_SLSH), KC_RSFT,           KC_UP,     KC_PGDN,
+			LT(4, KC_CAPS), KC_A,          LT(5, KC_S),   LT(3, KC_D),  LT(6, KC_F),   KC_G,  KC_H,    KC_J,         KC_K,            KC_L,            LT(7, KC_SCLN),  KC_QUOT,           KC_ENT,    KC_VOLU,
+		    KC_LSFT,        LCTL_T(KC_Z),  LSFT_T(KC_X),  LALT_T(KC_C), LGUI_T(KC_V),  KC_B,  KC_N,    LGUI_T(KC_M), LALT_T(KC_COMM), LSFT_T(KC_DOT),  LCTL_T(KC_SLSH), KC_RSFT,           KC_UP,     KC_VOLD,
 		    KC_LCTL,        KC_LALT,       KC_LGUI,                     KC_SPC,                        KC_RGUI,      MO(1),                                             KC_LEFT,           KC_DOWN,   KC_RIGHT),
 		[_FKEYS_LIGHTING] = LAYOUT_65_ansi_blocker( /* FN */
 			KC_TRNS,  KC_F1,    KC_F2,   KC_F3,  KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  KC_DEL,   KC_TRNS,
