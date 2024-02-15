68439abc958cea1f52f88cd9fb63169fa1d064d7
Chi Xing <xingchi90@gmail.com>
2024-02-15 05:37:38 -0800

    mouse keys

--- keyboards/kbdfans/kbd67/mkiirgb/keymaps/chixing/config.h
+++ keyboards/kbdfans/kbd67/mkiirgb/keymaps/chixing/config.h
@@ -1,4 +1,10 @@
 #pragma once
 
-// #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_TERM 200
+
+// Mouse key speed and acceleration.
+#define MOUSEKEY_DELAY          0
+#define MOUSEKEY_INTERVAL       8
+#define MOUSEKEY_WHEEL_DELAY    0
+#define MOUSEKEY_MAX_SPEED      4
+#define MOUSEKEY_TIME_TO_MAX    128
--- keyboards/kbdfans/kbd67/mkiirgb/keymaps/chixing/keymap.c
+++ keyboards/kbdfans/kbd67/mkiirgb/keymaps/chixing/keymap.c
@@ -8,6 +8,7 @@
 #define _BRACKETS 6
 #define _SYMBOLS_LEFT 7
 #define _GAMING 8
+#define _MOUSE 9
 
 // this is the kbd keyboard (black clear bottom)
 
@@ -19,7 +20,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		[_MAC] = LAYOUT_65_ansi_blocker( /* Base */
-			KC_GRV,         KC_1,          KC_2,          KC_3,         KC_4,          KC_5,  KC_6,    KC_7,         KC_8,            KC_9,            KC_0,            KC_MINS,  KC_EQL,  KC_BSPC,   TG(2),
+			LT(9, KC_GRV),  KC_1,          KC_2,          KC_3,         KC_4,          KC_5,  KC_6,    KC_7,         KC_8,            KC_9,            KC_0,            KC_MINS,  KC_EQL,  KC_BSPC,   TG(2),
 		    KC_TAB,         KC_Q,          KC_W,          KC_E,         KC_R,          KC_T,  KC_Y,    KC_U,         KC_I,            KC_O,            KC_P,            KC_LBRC,  KC_RBRC, KC_BSLS,   TG(8),
 			LT(4, KC_CAPS), KC_A,          LT(5, KC_S),   LT(3, KC_D),  LT(6, KC_F),   KC_G,  KC_H,    KC_J,         KC_K,            KC_L,            LT(7, KC_SCLN),  KC_QUOT,           KC_ENT,    KC_VOLU,
 		    KC_LSFT,        LCTL_T(KC_Z),  LSFT_T(KC_X),  LALT_T(KC_C), LGUI_T(KC_V),  KC_B,  KC_N,    LGUI_T(KC_M), LALT_T(KC_COMM), LSFT_T(KC_DOT),  LCTL_T(KC_SLSH), KC_RSFT,           KC_UP,     KC_VOLD,
@@ -72,5 +73,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 			KC_CAPS,        KC_A,          KC_S,          KC_D,         KC_F,          KC_G,  KC_H,    KC_J,         KC_K,            KC_L,            KC_SCLN,         KC_QUOT,           KC_ENT,    KC_PGUP,
 		    KC_LSFT,        KC_Z,          KC_X,          KC_C,         KC_V,          KC_B,  KC_N,    KC_M,         KC_COMM,         KC_DOT,          KC_SLSH,         KC_RSFT,           KC_UP,     KC_PGDN,
 		    KC_LCTL,        KC_LGUI,       KC_LALT,                     KC_SPC,                        KC_RGUI,      KC_RCTL,                                           KC_LEFT,           KC_DOWN,   KC_RIGHT),
+        [_MOUSE] = LAYOUT_65_ansi_blocker(
+            KC_TRNS,  KC_ACL0,  KC_ACL1, KC_ACL2,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
+            KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
+            KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_TRNS, KC_TRNS,          KC_TRNS,  KC_TRNS,
+            KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2, KC_BTN3, KC_TRNS, KC_TRNS,          KC_TRNS,  KC_TRNS,
+            KC_TRNS,  KC_TRNS,  KC_TRNS,                     KC_TRNS,                   KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS,  KC_TRNS),
 };
 
