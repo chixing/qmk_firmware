2a8367c7432c85c71569438417b4ab02f7191623
chi xing <xingchi90@gmail.com>
2026-07-13 20:57:46 -0700

    Unify F-key and modifier access

--- keyboards/kbdfans/kbd67/mkiirgb/keymaps/chixing/keymap.c
+++ keyboards/kbdfans/kbd67/mkiirgb/keymaps/chixing/keymap.c
@@ -26,8 +26,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             LALT_T(KC_GRV), KC_1,          KC_2,          KC_3,         KC_4,          KC_5,           KC_6,        KC_7,         KC_8,            KC_9,            KC_0,            KC_MINS,  KC_EQL,  KC_BSPC,   TG(_MAC),
             KC_TAB,         KC_Q,          KC_W,          KC_E,         KC_R,          KC_T,           KC_Y,        KC_U,         KC_I,            KC_O,            KC_P,            KC_LBRC,  KC_RBRC, KC_BSLS,   TO(_GAMING),
             LT(_CAPSLOCK, KC_ESC), KC_A,   LT(_SYMBOLS_RIGHT, KC_S), LT(_NUMPAD, KC_D), LT(_BRACKETS, KC_F), LT(_MOUSE, KC_G), KC_H, KC_J, KC_K, KC_L, LT(_SYMBOLS_LEFT, KC_SCLN), KC_QUOT, KC_ENT, KC_VOLU,
-            KC_LSFT,        LCTL_T(KC_Z),  LSFT_T(KC_X),  LALT_T(KC_C), LGUI_T(KC_V),  KC_B,           KC_N,        LGUI_T(KC_M), LALT_T(KC_COMM), LSFT_T(KC_DOT),  LCTL_T(KC_SLSH), KC_RSFT,           KC_UP,     KC_VOLD,
-            KC_LCTL,        KC_LGUI,       KC_LALT,                     KC_SPC,                        KC_RGUI,     MO(_FKEYS_LIGHTING),                                           KC_LEFT,           KC_DOWN,   KC_RIGHT),
+            KC_LSFT,        LCTL_T(KC_Z),  LSFT_T(KC_X),  LALT_T(KC_C), LGUI_T(KC_V),  LT(_FKEYS_LIGHTING, KC_B), KC_N, LGUI_T(KC_M), LALT_T(KC_COMM), LSFT_T(KC_DOT), LCTL_T(KC_SLSH), KC_RSFT, KC_UP, KC_VOLD,
+            KC_LCTL,        KC_LGUI,       KC_LALT,                     KC_SPC,                        KC_HYPR,     KC_MEH,                                                         KC_LEFT,           KC_DOWN,   KC_RIGHT),
         [_FKEYS_LIGHTING] = LAYOUT_65_ansi_blocker( /* FN */
             QK_BOOT,  KC_F1,    KC_F2,   KC_F3,  KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  KC_DEL,        KC_TRNS,
                     KC_TRNS,  RM_TOGG,  RM_NEXT, RM_HUEU,RM_HUED, RM_SATU, RM_SATD, RM_VALU, RM_VALD, KC_TRNS, KC_PSCR,  KC_SCRL, KC_PAUS, KC_TRNS,       KC_TRNS,
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TAB,         KC_Q,          KC_W,          KC_E,         KC_R,          KC_T,  KC_Y,    KC_U,         KC_I,            KC_O,            KC_P,            KC_LBRC,  KC_RBRC, KC_BSLS,   TO(_WINDOWS),
             KC_CAPS,        KC_A,          KC_S,          KC_D,         KC_F,          KC_G,  KC_H,    KC_J,         KC_K,            KC_L,            KC_SCLN,         KC_QUOT,           KC_ENT,    KC_VOLU,
             KC_LSFT,        KC_Z,          KC_X,          KC_C,         KC_V,          KC_B,  KC_N,    KC_M,         KC_COMM,         KC_DOT,          KC_SLSH,         KC_RSFT,           KC_UP,     KC_VOLD,
-            KC_LCTL,        KC_LGUI,       KC_LALT,                     KC_SPC,                        KC_RGUI,      KC_RCTL,                                           KC_LEFT,           KC_DOWN,   KC_RIGHT),
+            KC_LCTL,        KC_LGUI,       KC_LALT,                     KC_SPC,                        KC_HYPR,      KC_MEH,                                            KC_LEFT,           KC_DOWN,   KC_RIGHT),
         [_MOUSE] = LAYOUT_65_ansi_blocker(
             KC_TRNS,  MS_ACL0,   MS_ACL1,  MS_ACL2,   KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
             KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
--- keyboards/qwertykeys/qk65/r2/keymaps/chixing/keymap.c
+++ keyboards/qwertykeys/qk65/r2/keymaps/chixing/keymap.c
@@ -21,8 +21,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LALT_T(KC_GRV), KC_1,          KC_2,          KC_3,         KC_4,          KC_5,           KC_6,        KC_7,         KC_8,            KC_9,            KC_0,            KC_MINS, KC_EQL,  KC_BSPC, TG(_MAC),
         KC_TAB,         KC_Q,          KC_W,          KC_E,         KC_R,          KC_T,           KC_Y,        KC_U,         KC_I,            KC_O,            KC_P,            KC_LBRC, KC_RBRC, KC_BSLS, TO(_GAMING),
         LT(_NAV, KC_ESC), KC_A,        LT(_SYMBOLS_RIGHT, KC_S), LT(_NUMPAD, KC_D), LT(_BRACKETS, KC_F), LT(_MOUSE, KC_G), KC_H, KC_J, KC_K, KC_L, LT(_SYMBOLS_LEFT, KC_SCLN), KC_QUOT, KC_ENT, KC_VOLU,
-        KC_LSFT,        LCTL_T(KC_Z),  LSFT_T(KC_X),  LALT_T(KC_C), LGUI_T(KC_V),  KC_B,           KC_N,        LGUI_T(KC_M), LALT_T(KC_COMM), LSFT_T(KC_DOT), LCTL_T(KC_SLSH), KC_RSFT, KC_UP,   KC_VOLD,
-        KC_LCTL,        KC_LGUI,       KC_LALT,                     KC_SPC,                        HYPR(KC_NO), MO(_FN),                                                       KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LSFT,        LCTL_T(KC_Z),  LSFT_T(KC_X),  LALT_T(KC_C), LGUI_T(KC_V),  LT(_FN, KC_B), KC_N,        LGUI_T(KC_M), LALT_T(KC_COMM), LSFT_T(KC_DOT), LCTL_T(KC_SLSH), KC_RSFT, KC_UP,   KC_VOLD,
+        KC_LCTL,        KC_LGUI,       KC_LALT,                     KC_SPC,                        KC_HYPR,     KC_MEH,                                                         KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [_FN] = LAYOUT_65_ansi_blocker(
@@ -35,8 +35,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_NUMPAD] = LAYOUT_65_ansi_blocker(
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSLS, KC_7,    KC_8,    KC_9,    KC_PMNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PAST, KC_4,    KC_5,    KC_6,    KC_PPLS, KC_TRNS,          KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLU, KC_BRIU, KC_PSLS, KC_7,    KC_8,    KC_9,    KC_PMNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD, KC_BRID, KC_PAST, KC_4,    KC_5,    KC_6,    KC_PPLS, KC_TRNS,          KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_0,    KC_1,    KC_2,    KC_3,    KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,                   KC_TRNS, KC_TRNS,                              KC_TRNS, KC_TRNS, KC_TRNS
     ),
@@ -94,6 +94,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, TO(_BASE),
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_VOLU,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_VOLD,
-        KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                   KC_RGUI,  KC_RCTL,                              KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                   KC_HYPR,  KC_MEH,                               KC_LEFT, KC_DOWN, KC_RGHT
     ),
 };
