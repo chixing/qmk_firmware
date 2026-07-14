07dae28f3b5f6698daaf42e43e7d494c07046dc4
chi xing <xingchi90@gmail.com>
2026-07-14 00:26:19 -0700

    Remove unsafe KBD67 EEPROM clear binding

--- keyboards/kbdfans/kbd67/mkiirgb/keymaps/chixing/keymap.c
+++ keyboards/kbdfans/kbd67/mkiirgb/keymaps/chixing/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         [_FKEYS_LIGHTING] = LAYOUT_65_ansi_blocker( /* FN */
             QK_BOOT,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL,        KC_TRNS,
                     KC_TRNS,  RM_TOGG,  RM_NEXT, RM_HUEU, RM_HUED, RM_SATU, RM_SATD, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,
-                        KC_TRNS,  RM_VALU,  RM_VALD, RM_SPDU, RM_SPDD, KC_TRNS, KC_TRNS, KC_F4, KC_F5, KC_F6, KC_F11, KC_TRNS,          EE_CLR,        KC_TRNS,
+                        KC_TRNS,  RM_VALU,  RM_VALD, RM_SPDU, RM_SPDD, KC_TRNS, KC_TRNS, KC_F4, KC_F5, KC_F6, KC_F11, KC_TRNS,          KC_TRNS,        KC_TRNS,
             KC_TRNS,  KC_PSCR,  KC_SCRL, KC_PAUS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F12, KC_TRNS,          G(KC_TAB),     KC_MUTE,
             KC_TRNS,  KC_TRNS,  KC_TRNS,                  KC_TRNS,                   KC_TRNS,          KC_TRNS,  C(G(KC_LEFT)),    C(A(KC_TAB)),  C(G(KC_RIGHT))),
         [_MAC] = LAYOUT_65_ansi_blocker(
